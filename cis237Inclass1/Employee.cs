676f818b5e0a1e32e11d5ba085afb6c772336aa1
Jordan Koehler <kurai3@gmail.com>
2016-09-13 16:52:05 -0400

    Adds the Employee Class and changes some code under main

---
+++ cis237Inclass1/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class Employee
+    {
+
+        // Backing Fields
+
+        private string _firstName;
+        private string _lastName;
+        private decimal _weeklySalary;
+
+        // Properties
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+    
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = value; }
+        }
+
+        //Public Methods
+
+        public override string ToString()
+        {
+            return _firstName + " " + _lastName;
+        }
+
+        public decimal YearlySalary()
+        {
+            return this._weeklySalary * 52;
+        }
+        
+        //Constructor
+
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _weeklySalary = weeklySalary;
+        }
+
+        public Employee()
+        {
+
+        }
+    }
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,6 +10,33 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Employee myEmployee = new Employee();
+
+            myEmployee.FirstName = "Jordan";
+            myEmployee.LastName = "Koehler";
+            myEmployee.WeeklySalary = 200.00m;
+
+            Console.WriteLine(myEmployee.FirstName);
+            Console.WriteLine(myEmployee);
+
+            Employee[] employees = new Employee[10];
+
+            employees[0] = new Employee("Bob", "Jenkins", 500.00m);
+            employees[1] = new Employee("Bill", "Jenkins", 570.00m);
+            employees[2] = new Employee("Ben", "Jenkins", 506.00m);
+            employees[3] = new Employee("Bull", "Jenkins", 502.00m);
+            employees[4] = new Employee("Betty", "Jenkins", 504.00m);
+            employees[5] = new Employee("Barbara", "Jenkins", 520.00m);
+            employees[6] = new Employee("Butch", "Jenkins", 50.00m);
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    Console.WriteLine(employee.ToString() + " " + employee.YearlySalary());
+                }
+            }
+
         }
     }
 }
